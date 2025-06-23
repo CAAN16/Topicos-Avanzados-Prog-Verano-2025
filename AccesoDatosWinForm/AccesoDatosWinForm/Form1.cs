490771cb42785ba68a8d1848b85d9151cb0fa80c
Carlos Alexis Abrego <carlosalexisabrego@gmail.com>
2025-06-23 15:12:31 -0600

    Proyecto de acceso a datos con .NET

---
+++ AccesoDatosWinForm/AccesoDatosWinForm/Form1.cs
@@ -0,0 +1,27 @@
+using AccesoDatosWinForm.data;
+
+namespace AccesoDatosWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            var conn = new AccesoDatosMySql("localhost","nwind",
+                "root", "12345", 3306);
+
+            var resukt = conn.ejecutarSentencia(
+                "INSERT INTO categories (categoryname, description) " +
+                "VALUES ('Anime', 'Los comics japoneses')"
+                );
+
+            MessageBox.Show($"Filas afectdas {resukt}");
+
+
+        }
+    }
+}
