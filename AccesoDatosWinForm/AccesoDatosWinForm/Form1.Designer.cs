700c9f1a96b472e278afe1deab1cbcf93b88e77a
Carlos Alexis Abrego <carlosalexisabrego@gmail.com>
2025-06-25 09:04:43 -0600

    Se agrego metodo asincrono y un adapter.

--- AccesoDatosWinForm/AccesoDatosWinForm/Form1.Designer.cs
+++ AccesoDatosWinForm/AccesoDatosWinForm/Form1.Designer.cs
@@ -29,12 +29,16 @@
         private void InitializeComponent()
         {
             btnGuardar = new Button();
+            cboCAtegory = new ComboBox();
+            dgv = new DataGridView();
+            btnSaveAsync = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgv).BeginInit();
             SuspendLayout();
             // 
             // btnGuardar
             // 
-            btnGuardar.Location = new Point(521, 314);
-            btnGuardar.Margin = new Padding(2, 2, 2, 2);
+            btnGuardar.Location = new Point(523, 283);
+            btnGuardar.Margin = new Padding(2);
             btnGuardar.Name = "btnGuardar";
             btnGuardar.Size = new Size(90, 27);
             btnGuardar.TabIndex = 0;
@@ -42,21 +46,59 @@
             btnGuardar.UseVisualStyleBackColor = true;
             btnGuardar.Click += btnGuardar_Click;
             // 
+            // cboCAtegory
+            // 
+            cboCAtegory.FormattingEnabled = true;
+            cboCAtegory.Location = new Point(10, 10);
+            cboCAtegory.Margin = new Padding(2);
+            cboCAtegory.Name = "cboCAtegory";
+            cboCAtegory.Size = new Size(270, 28);
+            cboCAtegory.TabIndex = 1;
+            // 
+            // dgv
+            // 
+            dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv.Location = new Point(10, 52);
+            dgv.Margin = new Padding(2);
+            dgv.Name = "dgv";
+            dgv.RowHeadersWidth = 62;
+            dgv.Size = new Size(608, 203);
+            dgv.TabIndex = 2;
+            // 
+            // btnSaveAsync
+            // 
+            btnSaveAsync.Location = new Point(470, 315);
+            btnSaveAsync.Margin = new Padding(2);
+            btnSaveAsync.Name = "btnSaveAsync";
+            btnSaveAsync.Size = new Size(148, 27);
+            btnSaveAsync.TabIndex = 3;
+            btnSaveAsync.Text = "Guardar Async";
+            btnSaveAsync.UseVisualStyleBackColor = true;
+            btnSaveAsync.Click += btnSaveAsync_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(640, 360);
+            Controls.Add(btnSaveAsync);
+            Controls.Add(dgv);
+            Controls.Add(cboCAtegory);
             Controls.Add(btnGuardar);
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgv).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button btnGuardar;
+        private ComboBox cboCAtegory;
+        private DataGridView dgv;
+        private Button btnSaveAsync;
     }
 }
